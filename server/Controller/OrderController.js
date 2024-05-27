412a02b9bee74db44e7c4dd9cde535a5090d5c8c
Shobith S <shobithsugumar@gmail.com>
2024-05-27 13:21:26 +0530

    server

--- server/Controller/OrderController.js
+++ server/Controller/OrderController.js
@@ -14,3 +14,37 @@ exports.getOrders = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.OrderCheckout = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const { source } = req.body;
+    const cart = await Cart.findOne({ userId });
+    const user = await User.findOne({ _id: userId });
+    const email = user.email;
+
+    if (cart) {
+      const charge = await stripe.charges.create({
+        amount: cart.bill,
+        currency: "inr",
+        source: source,
+        receipt_email: email,
+      });
+      if (!charge) {
+        throw Error("Payment failed ");
+      }
+      if (charge) {
+        const order = await Order.create({
+          items: cart.items,
+          bill: cart.bill,
+        });
+        const data = await Cart.findByIdAndDelete({ _id: cart.id });
+        return res.status(201).send(order);
+      }
+    } else {
+      res.status(400).send("you dont have items in the cart ");
+    }
+  } catch (error) {
+    res.status(500).send("Something went wrong ");
+  }
+};
--- server/routers/OrderRoute.js
+++ server/routers/OrderRoute.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const orderController = require("../Controller/OrderController");
 
-router.get("/order/:id");
-router.post("/order/:id");
+router.get("/order/:id", orderController.getOrders);
+router.post("/order/:id", orderController.OrderCheckout);
+
+module.exports = router;
