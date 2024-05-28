74109216d388af862bc0b2922c76a9515cefedcf
Shobith S <shobithsugumar@gmail.com>
2024-05-28 10:42:21 +0530

    client added

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+function App() {
+  return (
+    <Container>
+      <h1>Hello everyone</h1>
+    </Container>
+  );
+}
+
+export default App;
